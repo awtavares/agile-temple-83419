e9afa808d61fd8fda055db1a6f81dbfbc40d8b4d
Anderson Tavares <contato@andersontavares.com>
2017-06-12 21:32:17 -0300

    email damanda

--- resources/views/email/createDemand.blade.php
+++ resources/views/email/createDemand.blade.php
@@ -1,4 +1,4 @@
 <p>Caro, {{$mediador->name}}</p>
 <p>Uma nova demanda foi cadastrada no Mentoring com o assunto "{{$demanda->subject}}".</p>
 <p>  </p>
-<p> Para direcioná-la a um mentor, gentileza acessar o <a href="http://agile-temple-83419.herokuapp.com">Sistema Mentoring.</a></p>
+<p> Para direcioná-la a um mentor, gentileza acessar o Sistema Mentoring.</a></p>
