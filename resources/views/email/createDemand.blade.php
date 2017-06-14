832e4049722892ccc42bd22062f1188a162f88a2
Anderson Tavares <contato@andersontavares.com>
2017-06-14 11:06:57 -0300

    link no email

--- resources/views/email/createDemand.blade.php
+++ resources/views/email/createDemand.blade.php
@@ -1,4 +1,4 @@
 <p>Caro {{$mediador->name}},</p>
 <p>Uma nova demanda foi direcionada para você no Mentoring.</p>
 <p> {{$demanda['subject']}} </p>
-<p> Para responde-la, gentileza acessar o sistema Mentoring. </p>
+<p> Para responde-la, gentileza acessar o <a href="{{ asset('http://agile-temple-83419.herokuapp.com') }}">Sistema Mentoring.</a> </p>
